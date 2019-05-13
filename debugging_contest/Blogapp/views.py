70929c889bc26b4e84633fc12672ddfe414da6db
hooong <tjrwns0529@gmail.com>
2019-05-13 17:00:21 +0900

    재밌게 풀어보세요

---
+++ debugging_contest/Blogapp/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from .form import PostForm
+from .models import Blog
+
+def index(request, post.id):
+    posts = Post.objects.all().order_by('-id')
+
+    return render(request, 'index.html', {'posts':posts})
+
+def detail(request, post.id):
+    post = get_object_or_404(Post, pk=post_id)
+
+    return render(request, 'detail.html', {'post':post})
+
+def delete(request, post.id):
+    post = get_object_or_404(Post, pk=post_id)
+    post.delete()
+    return redirect('index')
+
+def newpost(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.pub_date = timezone.now()
+        post.save()
+        return redirect('/post/'+post.id)
+    form = PostForm()
+    return render(request, 'new.html', {'forms':form})
+
+def edit(request,post.id):
+    post = get_object_or_404(Post, pk=post_id)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.pub_date = timezone.now()
+        post.save()
+        return redirect('post/'+str(post.id)
+    form = PostForm(instance=post)
+    return render(request, 'edit.html', {'form':form})
---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/hongseokjun/Desktop/debuging_contest/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
