ea839e81e2e595b8e9a016dd2cebcf0277fbfc02
chap95 <37801109+chap95@users.noreply.github.com>
2019-05-20 18:16:20 +0900

    Update models.py

--- debugging_contest/Blogapp/models.py
+++ debugging_contest/Blogapp/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField() 
-    # 찾아보니까 CharField의 속성에 max_length 값을 무조건 설정해줘야 하더라고 그래서 없앴음
     content = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
 
