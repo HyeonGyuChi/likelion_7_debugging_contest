d98d269ef7fac4f26558a24f2863b9a6908edf92
chap95 <37801109+chap95@users.noreply.github.com>
2019-05-20 18:16:51 +0900

    Update views.py

--- debugging_contest/Blogapp/views.py
+++ debugging_contest/Blogapp/views.py
@@ -5,7 +5,6 @@ from .models import Blog
 
 def index(request, post.id):
     posts = Post.objects.all()
-    #order_by('-id') 없애고 난 후에 최근에 쓴 글이 맨 위로 올라오게 해달라고 md 파일에 정리해 놓으면 좋을 듯
 
     return render(request, 'index.html', {'posts':posts})
 
